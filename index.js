6043c68da41e1453b34247de5c0dae74a4bb75c7
mahiuddinhabib <mh.habib137@gmail.com>
2022-05-03 23:13:36 +0600

    orders get and post

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ async function run(){
     try{
         await client.connect ();
         const serviceCollection = client.db('geniusCar').collection('service');
+        const orderCollection = client.db('geniusCar').collection('order');
 
         app.get('/service', async(req, res) => {
             const query = {};
@@ -41,6 +42,20 @@ async function run(){
             const result = await serviceCollection.deleteOne(query);
             res.send(result);
         });
+
+        app.post('/order', async (req, res)=>{
+            const order = req.body;
+            const result = await orderCollection.insertOne(order);
+            res.send(result);
+        });
+
+        app.get('/order', async (req, res)=>{
+            const email = req.query.email;
+            const query = {email};
+            const cursor = orderCollection.find(query);
+            const orders = await cursor.toArray();
+            res.send(orders);
+        })
     }
     finally{
 
